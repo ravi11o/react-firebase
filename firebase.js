090aac00509c89e6c728380fdea2fa762ded5489
ravi11o <ravi.suraj110@gmail.com>
2019-10-22 13:25:48 +0530

    setup firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import config from './config';
+
+var firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>
-          Welcome to Firebase
-        </h2>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h2>
+//           Welcome to Firebase
+//         </h2>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+
+  componentDidMount() {
+    console.log('setup firebase')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>
+            Welcome to Firebase
+          </h2>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
