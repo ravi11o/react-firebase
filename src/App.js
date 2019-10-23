1548aaa4ca53ccee5e0468f55313590e3c60b4ad
ravi11o <ravi.suraj110@gmail.com>
2019-10-23 12:22:49 +0530

    switch branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{ Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import 'bulma/css/bulma.css'
 import './App.css';
 import Home from './Home';
 import Posts from './Posts';
